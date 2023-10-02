7c1df5be4081a711f6fd1be2eb64d271d9062247
Assane Diallo <assane@pando2.com>
2023-10-02 21:10:15 +0200

    [serializers] Update CarSerializer fields to include all relevant attributes

--- PricePilotBackend/predictor/serializers.py
+++ PricePilotBackend/predictor/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
 from .models import Car
 
+
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
-        fields = '__all__'
+        fields = [
+            "name",
+            "price",
+            "year",
+            "origin",
+            "registration_date",
+            "technical_inspection",
+            "first_hand",
+            "mileage",
+            "fuel_type",
+            "transmission",
+            "num_doors",
+            "num_seats",
+            "power",
+            "co2_emission",
+            "length",
+            "trunk_volume",
+            "critair_rating",
+            "combined_consumption",
+        ]
