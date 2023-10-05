e63b7cee48a618baaf370e319ade567152cd6c27
Assane Diallo <assane@pando2.com>
2023-10-05 14:56:12 +0200

    add prediction and preprocessing methods

--- PricePilotBackend/predictor/predictor.py
+++ PricePilotBackend/predictor/predictor.py
@@ -1,4 +1,4 @@
-# import numpy as np  # Assuming we'll use numpy for handling data
+import numpy as np  # Assuming we'll use numpy for handling data
 import joblib  # For loading the trained model
 
 
@@ -7,15 +7,36 @@ class CarPricePredictor:
         self.model = joblib.load(model_path)  # Load our trained model
 
     def predict(self, input_data):
-        # Implement our prediction logic here using the loaded model
-        # For simplicity, let's assume the model always predicts a constant value for demonstration
-        # TODO: Replace this with our actual prediction logic
-        return 20000  # Dummy prediction
+        # Preprocess the input
+        preprocessed_input = self.preprocess_input(input_data)
+
+        # Reshape the input as the model expects a 2D array
+        preprocessed_input = np.array(preprocessed_input).reshape(1, -1)
+
+        # Predict using the loaded model
+        prediction = self.model.predict(preprocessed_input)[
+            0
+        ]  # Take the first prediction
+
+        # Post-process the prediction
+        post_processed_prediction = self.post_process_prediction(prediction)
+
+        return post_processed_prediction
 
     def preprocess_input(self, user_input):
-        # TODO: Implement any preprocessing needed for the input data
-        # For now, let's assume no preprocessing is needed
-        return user_input
+        # Define the order of features as in X_train and X_test
+        feature_order = ["year", "mileage", "power", "combined_consumption"]
+
+        # Extract relevant features from the input
+        input_features = [user_input.get(feature, 0) for feature in feature_order]
+
+        # Handle missing or incorrect values (e.g., empty strings, None)
+        input_features = [
+            0 if feature == "" or feature is None else feature
+            for feature in input_features
+        ]
+
+        return input_features
 
     def post_process_prediction(self, prediction):
         # TODO: Implement any postprocessing needed for the prediction
